8aeae8f85d423abf3f6e89b2b0eae897fa763fec
Marcel Reboucas <marcel.reboucas@inlocomedia.com>
2017-07-10 12:52:45 -0300

    Release 3.1.0

---
+++ MoPub/ILMMPInterstitialAdapter.h
@@ -0,0 +1,14 @@
+//
+//  ILMMPInterstitialAdapter.h
+//  InLocoMedia-IOS-Adapters
+//
+//  Created by Marcel Rebouças on 6/19/17.
+//  Copyright © 2017 InLocoMedia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MPInterstitialCustomEvent.h"
+
+@interface ILMMPInterstitialAdapter : MPInterstitialCustomEvent
+
+@end
---
+++ MoPub/ILMMPNativeAdapter.h
@@ -0,0 +1,14 @@
+//
+//  ILMMPNativeAdapter.h
+//  InLocoMedia-IOS-Adapters
+//
+//  Created by Marcel Rebouças on 6/20/17.
+//  Copyright © 2017 InLocoMedia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MPNativeCustomEvent.h"
+
+@interface ILMMPNativeAdapter : MPNativeCustomEvent
+
+@end
